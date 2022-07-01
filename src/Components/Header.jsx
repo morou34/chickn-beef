a9569bbc2b4206d5b7c128d2b8104499cc5d081d
rouane-mouaad <mouaadrio@gmail.com>
2022-07-01 05:21:25 -0400

    text edit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div id='main'>
       <div className='header-heading'>
         <h3>It's Great Time for A Good Taste of Chicken Burger</h3>
-        <h1><span>BUGER</span>For<br/>WEEK</h1>
+        <h1><span>BUGER </span>For<br/>WEEK</h1>
         <p className='details'>Order This Fucking BURGER, I need money and you<br/> need to EAT, that's it. that's the truth.</p>
         <div className='header-btns'>
           <a href="#" className='header-btn'>Ordre</a>
